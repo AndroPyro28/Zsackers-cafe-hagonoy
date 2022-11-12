c236dfdcfcda9bc717309268b20ed3057778d9f1
Andropyro28 <menandroeugenio1028@gmail.com>
2022-11-12 15:40:03 +0800

    'branch-update'

--- client-side/src/components/inventory/InventoryRightContent.tsx
+++ client-side/src/components/inventory/InventoryRightContent.tsx
@@ -4,45 +4,25 @@ import CategoryModal from '../modals/admin/category/CategoryModal'
 import ProductCreateModal from '../modals/admin/product/ProductCreateModal'
 import InventoryTableRow from '../table/InventoryTableRow'
 import Product from './Product'
-import { useGetAllCategoryQuery } from '../../services'
-import Logic from './Logic'
+import FilterItems from './FilterItems'
+import { useGetAllProductQuery } from '../../services'
 
 
-function InventoryRightContent() {
+function InventoryRightContent({searchName}: {searchName: string}) {
 
   const [openCreateProductModal, setOpenCreateProductModal] = useState<boolean>(false)
   const [viewCategory, setViewCategory] = useState<boolean>(false)
+  const [categoryId, setterCategoryId] = useState<number>(0)
+  const [subcategoryId, setterSubCategoryId] = useState<number>(0)
+  const [setcategoryId, setterSetCategoryId] = useState<number>(0)
 
-  const {data: categories, isError, isLoading} = useGetAllCategoryQuery('')
-  const [categoryId, setCategoryId] = useState<number>(0)
-  const [subcategoryId, setSubCategoryId] = useState<number>(0)
-  const [setcategoryId, setSetCategoryId] = useState<number>(0)
-  const {handleChange} = Logic();
-
-    if(isLoading) {
-      return <></>
-    }
-
-  const category = () => {
-    return categories?.find(value => value.id === categoryId)
-  }
-
-  const subcategory = () => {
-    return category()?.sub_category?.find(value => value.id === subcategoryId)
-  }
-
-  const fetchCategories = categories?.map((category) => (
-    <option value={category.id} key={category.id}>{category.name}</option>
-  ))
-
-  const fetchSubCategories = category()?.sub_category.map((subcategory) => (
-    <option value={subcategory.id} key={subcategory.id}>{subcategory.name}</option>
-  ))
-
-  const fetchSetCategories = subcategory()?.set_category.map((setcategory) => (
-    <option value={setcategory.id} key={setcategory.id}>{setcategory.name}</option>
-  ))
-
+  const {data} = useGetAllProductQuery({
+    searchName,
+    categoryId,
+    subcategoryId,
+    setcategoryId,
+  });
+  
   return (
     <InventoryRightContentContainer>
       {
@@ -53,48 +33,17 @@ function InventoryRightContent() {
         viewCategory && <CategoryModal setViewCategory={setViewCategory}/>
       }
     
-      <FilterItemsContainer>
-        <FilterContainer>
-        <span>Select</span>
-          <select onChange={(e: React.ChangeEvent<HTMLSelectElement>) => handleChange(setCategoryId, e.target.value)}>
-            <option value="">Category</option>
-            {fetchCategories}
-          </select>
-        </FilterContainer>
-
-        <FilterContainer>
-          <span>Select</span>
-          <select onChange={(e: React.ChangeEvent<HTMLSelectElement>) => handleChange(setSubCategoryId, e.target.value)}>
-            <option value="">Sub-Category</option>
-            {fetchSubCategories}
-          </select>
-        </FilterContainer>
-
-        <FilterContainer>
-          <span>Select</span>
-          <select onChange={(e: React.ChangeEvent<HTMLSelectElement>) => handleChange(setSetCategoryId, e.target.value)}>
-            <option value="">Set-Category</option>
-            {fetchSetCategories}
-          </select>
-        </FilterContainer>
-
-        <ButtonContainer>
-          <div>
-            <button onClick={() => setViewCategory(true)}>
-              View category
-            </button>
-          </div>
-          <button onClick={() => setOpenCreateProductModal(true)}>
-            Add products <i className="fa-solid fa-plus plus"></i>
-          </button>
-        </ButtonContainer>
-
-        <div className="pagination">
-        <i className="fa-solid fa-chevron-left left" onClick={() => alert("")}></i>
-          <span>{`${1} / ${1}`} </span>
-        <i className="fa-solid fa-chevron-right right"></i>
-        </div>
-      </FilterItemsContainer>
+      <FilterItems 
+      setOpenCreateProductModal={setOpenCreateProductModal}
+      setViewCategory={setViewCategory}
+      categoryId={categoryId}
+      subcategoryId={subcategoryId}
+      setcategoryId={setcategoryId}
+      setterCategoryId={setterCategoryId}
+      setterSubCategoryId={setterSubCategoryId}
+      setterSetCategoryId={setterSetCategoryId}
+  
+      />
 
       <InventoryTableRow />
       
